4d28f103de1d26a8225a592447f5bde3e6925014
Sugin Paladin <34408293+sugin223pl@users.noreply.github.com>
2020-05-27 14:11:52 +0300

    Added DIRECTORY_SEPARATOR

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require 'vendor/autoload.php';
-require 'extract.php';
+require __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+require __DIR__ . DIRECTORY_SEPARATOR . 'Extract.php';
 
 use App\Extract;
 
